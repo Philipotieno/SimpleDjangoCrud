af3a1e4b35fac8dda34763f5a93c19bea9c12888
philipotieno <tmitchellb007@gmail.com>
2020-05-28 18:02:13 +0300

    Deploy to heroku

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
 
     # Installed apps
     'rest_framework',
@@ -94,13 +93,13 @@ REST_FRAMEWORK = {
     ),
 }
 
-
+ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'a3',
         'USER': 'mitch',
         'PASSWORD': 'mufasa2019',
@@ -152,3 +151,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
